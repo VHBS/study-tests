39d1bfa3f650d769705ce9d8e1da07e6120d9dcb
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-28 17:27:49 -0300

    setup: draft.js component created

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// import MyInput from './components/RichTextEditorFunctional/RichTextEditor';
+import RichEditorExample from './components/RichTextEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Draft.js</h1>
+      {/* <MyInput /> */}
+      <RichEditorExample />
+    </div>
+  );
+}
+
+export default App;
